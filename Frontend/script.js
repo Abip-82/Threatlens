daa91ba9787767c830cca286ee4df1f27871e70a
Abip-82 <abipmahat04@gmail.com>
2026-05-17 09:50:09 +0545

    added changes in script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -12,3 +12,24 @@ async function analyze(){
     const data = await res.json();
     document.getElementById("result").innerText = `${data.verdict} \n Risk Score: ${data.risk_score}\n\n` + data.reasons.join('\n');
 }
+
+function showPage(id){
+    document.querySelectorAll('.page-content').forEach(p => p.style.display =  'none');
+    document.getElementById(id).style.display = 'flex';
+}
+
+async function crypto(mode){
+    let text = document.getElementById("cryptotext").value;
+    let key = document.getElementById("cryptokey").value;
+    let display = document.getElementById("cryptoresult");
+
+    display.innerText = "Processing ...";
+
+    let response = await fetch('http://127.0.0.1:5000/crypto', {
+        method : 'POST',
+        headers : {'Content-Type' : 'application/json'},
+        body : JSON.stringify({text:text , key:key , mode: mode})
+    });
+    let data = await response.json();
+    display.innerText = "Result : " + data.result;
+}
